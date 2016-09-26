3cc9a46d0d07e88a6a3f3e3e154d39b06bac7d53
xiaokai <xiaokai@juanpi.com>
2016-09-26 17:52:52 +0800

    feat(netty): echo demo

--- codeFragment/src/main/java/nettyDemo/c01_discardDemo/DiscardServerHandler.java
+++ codeFragment/src/main/java/nettyDemo/c01_discardDemo/DiscardServerHandler.java
@@ -1,6 +1,5 @@
 package nettyDemo.c01_discardDemo;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -9,10 +8,22 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  */
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
+    //    @Override
+//    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+//        ByteBuf in = (ByteBuf) msg;
+//        try {
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());//DISPLAY DEMO
+//                System.out.flush();
+//            }
+//        } finally {
+//            ReferenceCountUtil.release(msg); // (2)
+//        }
+//    }
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        // Discard the received data silently.
-        ((ByteBuf) msg).release(); // (3)
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ctx.write(msg); // (1) ECHO demo
+        ctx.flush(); // (2)
     }
 
     @Override
--- codeFragment/src/main/java/nettyDemo/nettyMemo.md
+++ codeFragment/src/main/java/nettyDemo/nettyMemo.md
@@ -1,5 +1,10 @@
 # netty 的四个主要部分
-## 1.Bootstrap 配置并启动服务的类
+## 1.Bootstrap 
+配置并启动服务的类
 ## 2.Eventloop 
-## 3.Channel 核心部分，处理连接
+NioEventLoopGroup is a multithreaded event loop that handles I/O operation. 
+Netty provides various EventLoopGroup implementations for different kind of transports
+## 3.Channel 
+核心部分，处理连接 
+Channel是理解和使用Netty的核心
 ## 4.Buffer 缓冲相关类，对NIO Buffer做了一些封装
