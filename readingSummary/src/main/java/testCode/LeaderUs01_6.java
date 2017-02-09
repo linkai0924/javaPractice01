b99b2e0765fadfa0e6091fc22112b1ded602d5ee
xiaokai <xiaokai@juanpi.com>
2017-02-09 09:57:14 +0800

    暂存

---
+++ readingSummary/src/main/java/testCode/LeaderUs01_6.java
@@ -0,0 +1,25 @@
+package testCode;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by xiaokai on 2017/2/7.
+ */
+public class LeaderUs01_6 {
+
+    public static void main(String[] args) {
+        //Arrays.parallelSort在数组超过多少时候才开启并行排序？采用位运算，给出推导过程
+        int[] list = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            list[i] = random.nextInt(100);
+        }
+        System.out.println(list);
+        Arrays.parallelSort(list);
+        Arrays.sort(list);
+        System.out.println(list);
+
+
+    }
+}
--- readingSummary/src/main/java/testCode/Salary.java
+++ readingSummary/src/main/java/testCode/Salary.java
@@ -1,6 +1,6 @@
 package testCode;
 
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by xiaokai on 2017/2/8.
@@ -12,10 +12,42 @@ public class Salary {
 
     public static void main(String[] args) {
         Random random = new Random();
-        Salary salary = new Salary();
-        salary.baseSalary = random.nextInt(100) % (100 - 5 + 1) + 5;
-        salary.bonus = random.nextInt(10);
+        List<Salary> salaryList = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            Salary salary = new Salary();
+            salary.baseSalary = random.nextInt(100) % (100 - 5 + 1) + 5;
+            salary.bonus = random.nextInt(10);
+            salary.name = getRandomString(5);
+            salaryList.add(salary);
+        }
 
-        System.out.println();
+        long begin = System.currentTimeMillis();
+        Collections.sort(salaryList, new Comparator<Salary>() {
+            @Override
+            public int compare(Salary o1, Salary o2) {
+                if ((o1.baseSalary * 13 + o1.bonus) > (o2.baseSalary * 13 + o2.bonus)) {
+                    return -1;
+                } else if ((o1.baseSalary * 13 + o1.bonus) < (o2.baseSalary * 13 + o2.bonus)) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        });
+        System.out.println("used time = " + (System.currentTimeMillis() - begin));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(salaryList.get(i).name);
+        }
+    }
+
+    static String getRandomString(int length) {
+        String base = "abcdefghijklmnopqrstuvwxyz";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
     }
 }
