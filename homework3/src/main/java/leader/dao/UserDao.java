9c6ab1639d1d7a5f0bf3140eed858599559dd8b4
linkai <linkai0924@gmail.com>
2017-02-15 06:12:03 +0800

    leader 学习

---
+++ homework3/src/main/java/leader/dao/UserDao.java
@@ -0,0 +1,15 @@
+package leader.dao;
+
+import leader.bean.User;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Lazy
+@Order(10)
+@Component("userDao")
+public class UserDao implements AbstractDAO<User> {
+    public UserDao() {
+        System.out.println(this + " order " + 10);
+    }
+}
---
+++ homework3/src/main/java/leader/service/UserService.java
@@ -0,0 +1,9 @@
+package leader.service;
+
+import leader.bean.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService extends AbstractService<User> {
+
+}
