42799177a514806d5dc660683ad7033060fab868
xiaokai <xiaokai@juanpi.com>
2017-02-15 15:15:19 +0800

    fix(homework3): 本地提交

--- homework3/src/main/java/leader/service/UserService.java
+++ homework3/src/main/java/leader/service/UserService.java
@@ -1,18 +1,22 @@
 package leader.service;
 
 import leader.bean.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserService extends leader.service.AbstractService<User> {
 
+    @Autowired
+    private UserService userService;
 
     public void login() {
         System.out.println("login");
+//        userService.createUser(user);
     }
 
-    public void createUser(User user){
-
+    public void createUser(User user) {
+        userService.createUser(user);
     }
 
 }
