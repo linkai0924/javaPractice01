2e32f4faf00b95d82fcbec02f0f4dcd7ba19a9f5
xiaokai <xiaokai@juanpi.com>
2017-03-07 17:46:00 +0800

    httpserver

--- homework3/src/main/java/leader/controller/DispatcherController.java
+++ homework3/src/main/java/leader/controller/DispatcherController.java
@@ -1,11 +1,16 @@
 package leader.controller;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  * Created by linkai on 2017/3/3.
  */
 public class DispatcherController {
 
-    public void handleGetRequest(String requestHeader) {
+    public JSONObject handleGetRequest(String requestHeader) {
         int index = requestHeader.indexOf("/?");
         if (index == -1) {
             System.out.println("GET参数是：" + "null");
@@ -15,19 +20,25 @@ public class DispatcherController {
             String condition = requestHeader.substring(begin, end);
             System.out.println("GET参数是：" + condition);
         }
+        return new JSONObject();
     }
 
-    public void handlePostRequest(String requestHeader) {
-        int begin = requestHeader.indexOf("Content-Lengh:") + "Content-Length:".length();
-        String postParamterLength = requestHeader.substring(begin).trim();
-        int contentLength = Integer.parseInt(postParamterLength);
-        System.out.println("POST参数长度是：" + Integer.parseInt(postParamterLength));
-//        StringBuilder sb = new StringBuilder();
-//        if (contentLength > 0) {
-//            for (int i = 0; i < contentLength; i++) {
-//                sb.append((char) bd.read());
-//            }
-//            System.out.println("POST参数是：" + sb.toString());
-//        }
+    public JSONObject handlePostRequest(String requestHeader, BufferedReader bd) {
+        try {
+            //content-length
+            int begin = requestHeader.indexOf("content-length") + "Content-Length:".length();
+            String postParamterLength = requestHeader.substring(begin).trim();
+            int contentLength = postParamterLength.length();
+            StringBuilder sb = new StringBuilder();
+            if (contentLength > 0) {
+                for (int i = 0; i < contentLength; i++) {
+                    sb.append((char) bd.read());
+                }
+                System.out.println("POST参数是：" + sb.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject();
     }
 }
--- homework3/src/main/java/leader/server/HTTPServer.java
+++ homework3/src/main/java/leader/server/HTTPServer.java
@@ -1,7 +1,8 @@
 package leader.server;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import leader.controller.DispatcherController;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,8 +19,7 @@ import java.net.Socket;
 public class HTTPServer {
 
 
-    @Autowired
-    private DispatcherController dispatcherController;
+    private DispatcherController dispatcherController = new DispatcherController();
 
     /**
      * server处理方法
@@ -32,27 +32,35 @@ public class HTTPServer {
             while (true) {
                 Socket socket = ss.accept();
                 BufferedReader bd = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println(JSON.toJSONString(socket));
                 /**
                  * 接受HTTP请求
                  */
                 String requestHeader;
+                JSONObject jsonObject = new JSONObject();
                 while ((requestHeader = bd.readLine()) != null && !requestHeader.isEmpty()) {
-                    System.out.println(requestHeader);
+                    System.out.println(JSON.toJSONString(requestHeader));
                     /**
                      * 处理GET请求
                      */
                     if (requestHeader.startsWith("GET")) {
-                        dispatcherController.handleGetRequest(requestHeader);
+                        jsonObject = dispatcherController.handleGetRequest(requestHeader);
                     }
                     /**
                      * 处理POST请求
                      */
-                    if (requestHeader.startsWith("POST /")) {
-                        dispatcherController.handlePostRequest(requestHeader);
+                    if (requestHeader.startsWith("POST")) {
+                        jsonObject = dispatcherController.handlePostRequest(requestHeader,bd);
                     }
                 }
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < 5; i++) {
+                    sb.append((char) bd.read());
+                }
+                System.out.println("POST参数是：" + sb.toString());
                 //发送回执
                 PrintWriter pw = new PrintWriter(socket.getOutputStream());
+                pw.print(jsonObject);
                 pw.println("HTTP/1.1 200 OK");
                 pw.println("Content-type:text/html");
                 pw.println();
